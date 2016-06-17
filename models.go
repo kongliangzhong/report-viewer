87a3a482d031b6aa1603aa3f7217f428f79256be
kongliangzhong <kongliangzhong@gmail.com>
2016-06-17 18:26:42 +0800

    timeline impl begin

--- models.go
+++ models.go
@@ -1,12 +1,14 @@
 package main
 
 type ReportItem struct {
-    Id         string
-    Title      string
-    Desc       string
-    CreateTime string
-    TableName  string
-    HandlerKey string
+    Id            string
+    Title         string
+    Desc          string
+    CreateTime    string
+    TableName     string
+    HandlerKey    string
+    ShowTimeLine  bool   // timeline support
+    TimeLineField string
 }
 
 // type ChartDataRaw struct {
@@ -18,4 +20,10 @@ type ReportItem struct {
 type PieDataItem struct {
     Value int    `json:"value"`
     Name  string `json:"name"`
+}
+
+type ChartData struct {
+    Title       string
+    HandlerKey  string
+    OptionsData []interface{}
 }
